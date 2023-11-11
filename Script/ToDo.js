1518f35ab6cceb5dcfa84dc98e21389663b172ff
amethystcoder <amethystguitaristj@gmail.com>
2023-11-11 22:49:18 +0000

    added local storage for persistent todo stores

--- Script/ToDo.js
+++ Script/ToDo.js
@@ -1,9 +1,17 @@
+let to_do_list = []
+
 // Function to add a task to the list
 function addTask() {
 const taskText = taskInput.value.trim();
+const timeToComplete = parseInt(document.getElementById("time-to-complete").value)
+const minOrSec = document.getElementById("min-or-sec").value
+
 if (taskText !== "") {
      const newTask = document.createElement("li");
-     newTask.textContent = taskText;
+     if(isNaN(timeToComplete))
+          newTask.textContent = `${taskText} - should finish in ${0} ${minOrSec}`
+     else
+          newTask.textContent = `${taskText} - should finish in ${timeToComplete} ${minOrSec}`
 
      const deleteButton = document.createElement("button");
      deleteButton.textContent = "Delete";
@@ -11,51 +19,72 @@ if (taskText !== "") {
           taskList.removeChild(newTask);
      });
 
+     to_do_list.push({task:taskText,finishingtime:timeToComplete,min_or_sec:minOrSec})
+
      newTask.appendChild(deleteButton);
      taskList.appendChild(newTask);
-     taskInput.value = "";
+     taskInput.value = "";     
 }
 }
 
 // Function to save a list
 function saveList() {
-const listTitleInput = document.getElementById("listTitle");
-const taskList = document.getElementById("taskList");
-const savedListItems = document.getElementById("savedListItems");
 
-const listTitle = listTitleInput.value.trim();
-if (listTitle !== "") {
-     const listContainer = document.createElement("div");
-     const newList = document.createElement("ul");
-     newList.innerHTML = taskList.innerHTML;
-     listContainer.appendChild(document.createElement("h2")).textContent = listTitle;
+     const listTitleInput = document.getElementById("listTitle");
+     const taskList = document.getElementById("taskList");
+     const savedListItems = document.getElementById("savedListItems");
 
-     const editButton = document.createElement("button");
-     editButton.textContent = "Edit";
-     editButton.addEventListener("click", () => {
-          taskList.innerHTML = newList.innerHTML;
-          taskInput.value = "";
-          listTitleInput.value = listTitle;
-          saveListBtn.textContent = "Update List";
-          saveListBtn.removeEventListener("click", saveList);
-          saveListBtn.addEventListener("click", () => {
-               updateList(listContainer);
+     const listTitle = listTitleInput.value.trim();
+     if (listTitle !== "") {
+
+          let localstorage = window.localStorage
+          let prevTodos = localstorage.getItem("to-dos")
+          if(prevTodos != null && prevTodos != ""){
+               let parsedPrevTodos = JSON.parse(prevTodos)
+               parsedPrevTodos.push({todo_title:listTitle,todoTasks:to_do_list})
+               localstorage.removeItem("to-dos")
+               localstorage.setItem("to-dos",JSON.stringify(parsedPrevTodos))
+               console.log(prevTodos)
+          }
+          else{
+               let newTodoTitleList = []
+               newTodoTitleList.push({todo_title:listTitle,todoTasks:to_do_list})
+               localstorage.setItem("to-dos",JSON.stringify(newTodoTitleList))
+          }
+
+
+          const listContainer = document.createElement("div");
+          const newList = document.createElement("ul");
+          newList.innerHTML = taskList.innerHTML;
+          listContainer.appendChild(document.createElement("h2")).textContent = listTitle;
+
+          const editButton = document.createElement("button");
+          editButton.textContent = "Edit";
+          editButton.addEventListener("click", () => {
+               taskList.innerHTML = newList.innerHTML;
+               taskInput.value = "";
+               listTitleInput.value = listTitle;
+               saveListBtn.textContent = "Update List";
+               saveListBtn.removeEventListener("click", saveList);
+               saveListBtn.addEventListener("click", () => {
+                    updateList(listContainer);
+               });
           });
-     });
 
-     const deleteButton = document.createElement("button");
-     deleteButton.textContent = "Delete";
-     deleteButton.addEventListener("click", () => {
-          savedListItems.removeChild(listContainer);
-     });
+          const deleteButton = document.createElement("button");
+          deleteButton.textContent = "Delete";
+          deleteButton.addEventListener("click", () => {
+               savedListItems.removeChild(listContainer);
+          });
 
-     listContainer.appendChild(editButton);
-     listContainer.appendChild(deleteButton);
-     listContainer.appendChild(newList);
-     savedListItems.appendChild(listContainer);
-     listTitleInput.value = "";
-     taskList.innerHTML = "";
-}
+          listContainer.appendChild(editButton);
+          listContainer.appendChild(deleteButton);
+          listContainer.appendChild(newList);
+          savedListItems.appendChild(listContainer);
+          listTitleInput.value = "";
+          taskList.innerHTML = "";
+          to_do_list = []
+     }
 }
 
 // Function to update a saved list
