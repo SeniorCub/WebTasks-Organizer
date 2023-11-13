f473d394d18d8affba927993b0665757d0f012eb
seniorcub <farinderif@gmail.com>
2023-11-13 16:05:17 +0100

    Worked on the display of the clock not to show at once

--- Script/time.js
+++ Script/time.js
@@ -17,3 +17,19 @@ function updateTime() {
 
      // Refresh time every 1 second (1000 milliseconds)
      setInterval(updateTime, 1000);
+
+let analog = document.querySelector(".analog").addEventListener("click", moveToAnalog)
+let digital = document.querySelector(".digital").addEventListener("click", moveToDigital)
+
+function moveToDigital() {
+     document.querySelector(".outer-analog-container").style.display = 'none'
+     document.querySelector(".timee").style.display = 'block'
+     document.querySelector(".digital").classList.add("active")
+     document.querySelector(".analog").classList.remove("active")
+}
+function moveToAnalog() {
+     document.querySelector(".timee").style.display = 'none'
+     document.querySelector(".outer-analog-container").style.display = 'block'
+     document.querySelector(".analog").classList.add("active")
+     document.querySelector(".digital").classList.remove("active")
+}
