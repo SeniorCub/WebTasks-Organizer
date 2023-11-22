e27f1e68b65b7a7677cd211a4528781ae8f85ca3
seniorcub <farinderif@gmail.com>
2023-11-22 11:16:58 +0100

    Tried fetching an api for the currency calculator

--- CalcSrc/script/currency.js
+++ CalcSrc/script/currency.js
@@ -1,29 +1,5 @@
-import CurrencyAPI from '@everapi/currencyapi-js';
+import currencyapi from '@everapi/currencyapi-js'
 
-const currencyApi = new CurrencyAPI('YOUR_API_KEY_GOES_HERE');
-CurrencyAPI.convert({
-    value: 12
-}).then(response => {
-    console.log(response)
-});
-const apiUrl = 'https://api.exchangeratesapi.io/v1/latest?access_key=01193e9d6dc2c519cf8b235e36d67b4d'
+const client = new currencyapi('cur_live_sg17uxxZrxJXUUoLr5Ike0jLFBGml50g55ecRVsV')
 
-fetch(apiUrl)
-.then(response => { 
-     // Check if the request was successful (status code 200-299)
-     if (!response.ok) {
-     throw new Error(`HTTP error! Status: ${response.status}`);
-     }
-     
-     // Parse the response as JSON
-     return response.json();
-})
-.then(data => {
-     // Handle the data from the API
-     console.log(data);
-})
-.catch(error => {
-     // Handle errors
-     console.error('Fetch error:', error);
-});
-
+await client.latest()
