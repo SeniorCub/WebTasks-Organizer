24b2ddb961d479754a16e7fc9962519701a02f16
Reuben Ifeoluwa Farinde <99650027+SeniorCub@users.noreply.github.com>
2023-11-30 18:11:33 +0100

    working on the form for sign up for users

--- Script/script.js
+++ Script/script.js
@@ -1,72 +1,144 @@
-// Attach event listeners to elements with the "user" or "start" class
-const userElements = document.querySelector(".user").addEventListener('Click',sign())
+// Next on Form one
+const next1 = document.querySelector('.next1').addEventListener("click", btn_next1);
+// Next and Back on Form 2
+const next2 = document.querySelector('.next2').addEventListener("click", btn_next2);
+const back2 = document.querySelector('.back2').addEventListener("click", btn_back2);
+// Next and Back on Form 3
+const next3 = document.querySelector('.next3').addEventListener("click", btn_next3);
+const back3 = document.querySelector('.back3').addEventListener("click", btn_back3);
+// Back and Next on Form 4
+const back4 = document.querySelector('.back4').addEventListener("click", btn_back4);
+const next4 = document.querySelector('.next4').addEventListener("click", btn_next4);
 
-function sign() {
-    document.querySelector('#mod').innerHTML = `
-    <div class="modal modal-lg fade" id="exampleModalToggle" class="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered ">
-         <div class="modal-content bg-dark">
-              <div class="modal-header">
-                   <h1 class="modal-title fs-5 text-prim" id="exampleModalToggleLabel">
-                        Sign In
-                   </h1>
-                   <button type="button" class="btn-close text-light" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-    
-              <div class="modal-body">
-                   <form>
-                        <div class="d-flex gap-2">
-                             <div class="form-group" style="width: 100%;">
-                                  <label for="firstName">First Name</label>
-                                  <input type="text" class="form-control" id="firstName" placeholder="Enter your first name">
-                              </div>
-                              <div class="form-group"  style="width: 100%;">
-                                  <label for="lastName">Last Name</label>
-                                  <input type="text" class="form-control" id="lastName" placeholder="Enter your last name">
-                              </div>
-                        </div>
-    
-                        <div class="d-flex justify-content-around">
-                             <div class="form-group">
-                                  <label for="email">Email Address</label>
-                                  <input type="email" class="form-control" id="emaill" placeholder="example@email.com">
-                              </div>
-    
-                              <div class="form-group">
-                                  <label for="phone">Phone Number</label>
-                                  <input type="tel" class="form-control" id="phone" placeholder="+1234567890">
-                             </div>
-                             <div class="form-group">
-                                  <label for="birthdate">Date of Birth</label>
-                                  <input type="date" class="form-control" id="birthdate">
-                             </div>
-                        </div>
-    
-                        <div class="">
-                             <div class="form-group">
-                                 <label for="username">Username</label>
-                                 <input type="text" class="form-control" id="username" placeholder="Enter username">
-                             </div>
-    
-                             <div class="d-flex justify-content-around">
-                                  <div class="form-group">
-                                       <label for="password">Password</label>
-                                       <input type="password" class="form-control" id="CrPwd" placeholder="Enter password">
-                                  </div>
-                                  <div class="form-group">
-                                       <label for="password">Confirm Password</label>
-                                       <input type="password" class="form-control" id="CoPwd" placeholder="Confirm password">
-                                  </div>
-                             </div>
-                        </div>
-                   </form>
-              </div>
-    
-              <div class="modal-footer">
-                   <button class="btn btn-prim save" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" id="save">Save</button>
-              </div>
-         </div>
-    </div>
-    </div> 
-    `
-}
+// Inputs on Form 1
+let firstName = document.querySelector('#first_name');
+let lastName = document.querySelector('#last_name');
+let userName = document.querySelector('#user_name');
+// Inputs on Form 2
+let email = document.querySelector('#email');
+let phone = document.querySelector('#phone');
+let age = document.querySelector('#age');
+// Inputs on Form 3
+let crPwd = document.querySelector('#CrPwd');
+let coPwd = document.querySelector('#CoPwd');
+// Inputs on Form 4
+let ckbox = document.querySelector('#ckbox');
+
+// For Input filter
+const fullNamePattern = /^[A-Za-z\s'\-]+$/;
+// Define a regular expression pattern for email validation.
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+// Step 1
+function btn_next1() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
+     const line2 =document.querySelector('.line2')
+     
+     if (firstName.value == "" || lastName.value == "" || userName.value == "") {
+          alert('Error');
+     } else if (!fullNamePattern.test(firstName.value) || !fullNamePattern.test(lastName.value) || !fullNamePattern.test(userName.value)) {
+          alert('Either First name or Last name or UserName has Invalid inputs');
+     } else {
+          line2.style.display = 'block'
+          form1.style.left = '-600px'
+          form2.style.left = '130px'
+          form3.style.left = '600px'
+          form4.style.left = '1070px'
+     }
+};
+// Step 2
+function btn_next2() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
+     const line3 =document.querySelector('.line3')
+     const ageValue = parseInt(age.value);
+
+     if (email.value == "" || phone.value == "" || age.value == "") {
+          alert('Error')
+     } else if (!emailPattern.test(email.value) ) {
+          alert('Invalid email phone');
+     } else if (isNaN(ageValue) || ageValue < 0 || ageValue > 150) {
+          alert('Invalid age');
+     } else {
+     line3.style.display = 'block'
+     form1.style.left = '-1070px'
+     form2.style.left = '-600px'
+     form3.style.left = '130px'
+     form4.style.left = '600px'
+     }
+};
+function btn_back2() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
+     const line2 =document.querySelector('.line2')
+
+     line2.style.display = 'none'
+     form1.style.left = '130px'
+     form2.style.left = '600px'
+     form3.style.left = '1070px'
+     form4.style.left = '1540px'
+};
+// Step 3
+function btn_back3() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
+     const line3 =document.querySelector('.line3')
+
+     line3.style.display = 'none'
+     form1.style.left = '-600px'
+     form2.style.left = '130px' 
+     form3.style.left = '600px'
+     form4.style.left = '1070px'
+};
+function btn_next3() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
+     const line4 =document.querySelector('.line4')
+     if (crPwd.value == "" || coPwd.value == "") {
+          alert('Error')
+     }else if (crPwd.value.length !== 8 || coPwd.value.length !== 8) {
+          alert('Password must be exactly 8 characters long');
+     } else if (crPwd.value === coPwd.value) {
+          // Proceed to the next step
+          line4.style.display = 'block'
+          form1.style.left = '-1540px'
+          form2.style.left = '-1070px' 
+          form3.style.left = '-600px'
+          form4.style.left = '130px'
+     } else {
+          alert('Passwords do not match');
+     }
+};
+// Step 4
+function btn_back4() {
+     const form1 = document.querySelector('.form1')
+     const form2 = document.querySelector('.form2')
+     const form3 = document.querySelector('.form3')
+     const form4 = document.querySelector('.form4')
+     const line4 =document.querySelector('.line4')
+
+     line4.style.display = 'none'
+     form1.style.left = '-1070px'
+     form2.style.left = '-600px'
+     form3.style.left = '130px'
+     form4.style.left = '600px'
+};
+function btn_next4() {
+     if (ckbox.checked) {
+          // For external link
+          window.location.href = "https://www.github.com/seniorcub"
+     } else {
+          alert('Error')
+     }
+};
---
+++ Script/validation.js
@@ -0,0 +1,25 @@
+// const save = document.querySelector("#save").addEventListener("Click", () => {
+//      const firstName = document.querySelector("#firstName").value
+//      const lastName = document.querySelector("#lastName").value
+//      const emaill = document.querySelector("#emaill").value
+//      const phone = document.querySelector("#phone").value
+//      const birthdate = document.querySelector("#birthdate").value
+//      const username = document.querySelector("#username").value
+//      const CrPwd = document.querySelector("#CrPwd").value
+//      const CoPwd = document.querySelector("#CoPwd").value
+//      console.log(firstName)
+//      console.log(lastName)
+//      console.log(emaill)
+//      console.log(phone)
+//      console.log(birthdate)
+//      console.log(username)
+//      console.log(CrPwd)
+//      console.log(CoPwd)
+//      alert("hey")
+// })
+
+// Attach event listeners to elements with the "user" or "start" class
+const userElements = document.querySelector(".user").addEventListener('Click',sign())
+ function sign() {
+     
+ }
