b03449e55f16accb1146731379141eb1b2857142
seniorcub <farinderif@gmail.com>
2024-01-23 11:06:28 +0100

    Added the loading effect to the time pages, remaining responsiveness

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 // Add a delay function
 function delay(milliseconds) {
      return new Promise(resolve => setTimeout(resolve, milliseconds));
-}
-
-// Call the delay function before displaying content
-async function initializeWebsite() {
+   }
+ 
+   // Call the delay function before displaying content
+   async function initializeWebsite() {
      // Show loading display
      document.getElementById('loading').style.display = 'flex';
-
-     // Add any other initialization code here
-
+ 
      // Wait for 2000 milliseconds (2 seconds) before displaying content
      await delay(2000);
-
+ 
      // Hide loading display
      document.getElementById('loading').style.display = 'none';
-
+ 
      // Display the content by removing the 'hidden' class
      document.getElementById('content').classList.remove('hidden');
-}
-
-// Run the initialization function when the window has finished loading
-window.addEventListener('load', initializeWebsite);
+   }
+ 
+   // Run the initialization function when the window has finished loading
+   window.addEventListener('load', initializeWebsite);
